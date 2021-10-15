12192e3491cc22a9ec3c357d1bac561d5f9b6ed0
Aleksey Danilevskiy <a.danilevsky@allfuneral.com>
2021-10-15 14:45:40 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM haskell:8.10.7-buster as build
+
+# Copying sources
+WORKDIR /tmp/build
+COPY    . ./
+# Building service
+RUN     stack build --system-ghc --copy-bins
+# Collecting shared libraries
+WORKDIR /root/.local/bin
+RUN     mkdir /tmp/lib && for lib in `ldd ./imps | grep "=>"| awk '{print $3;}' | egrep -v 'libm.so.6|libc.so.6|ld-linux-x86-64.so.2|libresolv.so.2'`; do cp $lib /tmp/lib/; done
+
+# Building imps docker image
+FROM    busybox:stable-glibc as app
+# Copying imps binary
+COPY    --from=build /root/.local/bin/http-request-log-service /usr/local/bin/http-request-log-service
+# Copying shared library dependencies
+COPY    --from=build /tmp/lib /lib
+# Running service
+EXPOSE  7777
+ENTRYPOINT ["http-request-log-service"]
---
+++ src/Main.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.Aeson (decode)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Aeson.Types (Value, emptyObject)
+import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.UTF8 (toString)
+import Data.Maybe
+import Data.Time.LocalTime (getZonedTime)
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp
+import System.IO
+
+main :: IO ()
+main = run 7777 requestLogger
+
+logFile :: FilePath
+logFile = "requests.log"
+
+requestLogger :: Application
+requestLogger request respond = do
+  withFile
+    logFile
+    AppendMode
+    (\handle -> do
+       currentTime <- getZonedTime
+       hPutStrLn handle $ "========== " <> show currentTime <> " =========="
+       hPrint handle request
+       hPutStrLn handle "Body:"
+       bodyBytes <- strictRequestBody request
+       let headers = requestHeaders request
+           contentType = toString $ fromMaybe "" (lookup hContentType headers)
+       if contentType == "application/json"
+         then do
+           let bodyJson = fromMaybe emptyObject . decode $ bodyBytes
+           hPutStrLn handle (toString . toStrict . encodePretty $ bodyJson)
+         else hPutStrLn handle (toString . toStrict $ bodyBytes)
+       hPutStrLn handle ""
+    )
+  respond $ responseLBS status200 [] ""
